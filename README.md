6e2fba5409be77fb6b805442d1314f02ccd21f5c
Bernhard Hering <bernhard.hering@slub-dresden.de>
2020-06-16 16:30:36 +0200

    added setupfile, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # pymarc2jsonl
 MARC21 to MarcXchange formatted ld-JSON and reverse converter based on pymarc
+
+## requirements
+  - [es2json](https://github.com/slub/es2json)
+  - [six](https://github.com/benjaminp/six)
+  - [pymarc](https://gitlab.com/pymarc/pymarc)
+
+## Installation
+Clone this Repository, cd into it and run:
+```
+python3 -m pip install --user .
+```
+## usage
+pymarc2jsonl and json2marc21 don't use any cmdline arguments, they only use stdout and stdin.
+
+e.g.:
+``
+pymarc2jsonl < binary_marc21.mrc > line_delmited_json.ldj
+```
+
+```
+json2marc21 < line_delimited_json.ldj > binary_marc21.mrc
+```
---
+++ setup.py
@@ -0,0 +1,38 @@
+"""
+simple downloader for saving a whole ElasticSearch-index, just the corresponding type or just a document into a line-delimited JSON-File
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='pymarc2jsonl',
+      version='0.0.1',
+      description='MARC21 to MarcXchange formatted ld-JSON and reverse converter based on pymarc',
+      url='https://github.com/slub/pymarc2jsonl',
+      author='Bernhard Hering',
+      author_email='bernhard.hering@slub-dresden.de',
+      license="Apache 2.0",
+      packages=[
+          'marc2jsonl',
+          'json2marc21'
+          ],
+      package_dir={
+          'marc2jsonl': 'marc2jsonl',
+          'json2marc21': 'json2marc21'
+          },
+      install_requires=[
+          'argparse>=1.4.0',
+          'pymarc>=4.0.0',
+          'six>=1.14.0',
+          'es2json>=0.0.1'
+      ],
+      python_requires=">=3.6.*",
+      entry_points={
+          "console_scripts": [
+              "pymarc2jsonl=marc2jsonl.marc2jsonl:main",
+              "json2marc21=json2marc21.json2marc21:main"
+              ]
+          }
+      )
