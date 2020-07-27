5abe065c2d50d6f8d4d189a73ee5c6a33d147ed3
Bernhard Hering <bernhard.hering@slub-dresden.de>
2020-07-27 17:28:02 +0200

    modified import to work with pip

--- json2marc21/json2marcxml.py
+++ json2marc21/json2marcxml.py
@@ -6,22 +6,24 @@
 import json
 import sys
 from pymarc import Record, Field, marcxml
-from json2marc21 import transpose_to_marc21
+from json2marc21.json2marc21 import transpose_to_marc21
 from es2json import isint
 
 
 def main():
-    sys.stdout.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+    sys.stdout.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<collection xmlns=\"http://www.loc.gov/MARC21/slim\">")
     sys.stdout.flush()
     for line in sys.stdin:
         try:
             record = json.loads(line)
             record = transpose_to_marc21(record)
-            sys.stdout.buffer.write(marcxml.record_to_xml(record,quiet=False))
+            sys.stdout.buffer.write(marcxml.record_to_xml(record,quiet=True,namespace=True))
             sys.stdout.flush()
         except UnicodeDecodeError as e:
             eprint("unicode decode error: {}".format(e))
             eprint(record)
+    sys.stdout.write("</collection>\n")
+    sys.stdout.flush()
 
 
 if __name__ == "__main__":
